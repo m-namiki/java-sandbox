f793c612adc2f18250653a65bc6b7cc743109923
m-namiki <masa1824@gmail.com>
2012-07-17 11:04:54 +0900

    DateUtilsにメソッドを追加しました。

--- src/test/java/jp/co/shantery/sandbox/util/DateUtilsTest.java
+++ src/test/java/jp/co/shantery/sandbox/util/DateUtilsTest.java
@@ -1,11 +1,13 @@
 package jp.co.shantery.sandbox.util;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 /**
@@ -34,4 +36,47 @@ public class DateUtilsTest {
 				is(DateUtils.getSystemDateString("yyyy/MM/dd")));
 	}
 
+	/**
+	 * {@link DateUtils#formatDateToString(Date, String)}のテストケースです。
+	 */
+	@Test
+	public void formatDateToString() {
+		DateTime dateTime = new DateTime(2012, 7, 1, 12, 0, 0);
+		assertThat("2012-07-01", is(DateUtils.formatDateToString(
+				dateTime.toDate(), "yyyy-MM-dd")));
+
+		assertThat("2012-07-01 12:00:00", is(DateUtils.formatDateToString(
+				dateTime.toDate(), "yyyy-MM-dd HH:mm:ss")));
+	}
+
+	/**
+	 * {@link DateUtils#parseDate(String, String)}のテストケースです。
+	 */
+	@Test
+	public void parseDate_日付が正しい場合() {
+		Date date = DateUtils.parseDate("2012-07-01", "yyyy-MM-dd");
+		DateTime dateTime = new DateTime(date);
+		assertThat(dateTime.toString("yyyy-MM-dd"), is("2012-07-01"));
+	}
+
+	/**
+	 * {@link DateUtils#parseDate(String, String)}のテストケースです。
+	 */
+	@Test
+	public void parseDate_日付が正しくない場合() {
+		assertThat(DateUtils.parseDate("2012-07-aa", "yyyy-MM-dd"), nullValue());
+		assertThat(DateUtils.parseDate("2011-07-01", "yyyy/MM/dd"), nullValue());
+	}
+
+	/**
+	 * {@link DateUtils#isDate(String, String)}のテストケースです。
+	 */
+	@Test
+	public void isDate() {
+		assertThat(DateUtils.isDate("2012-07-01", "yyyy-MM-dd"), is(true));
+		assertThat(DateUtils.isDate("2012/07/01", "yyyy/MM/dd"), is(true));
+		assertThat(DateUtils.isDate("2012-07-aa", "yyyy-MM-dd"), is(false));
+		assertThat(DateUtils.isDate("", "yyyy-MM-dd"), is(false));
+		assertThat(DateUtils.isDate(null, "yyyy-MM-dd"), is(false));
+	}
 }
